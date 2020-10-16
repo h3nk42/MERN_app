f5c3d080d275b2860b96f551e483e37426f46e3e
henk <hkripper@gmail.com>
2020-10-16 12:41:29 +0200

    Updates

---
+++ precache-manifest.92bc7c69af26bbb10567771aa0825703.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99deafac1a0a650b8d83e9ae3b42f545",
+    "url": "/ShareYourPlant/index.html"
+  },
+  {
+    "revision": "087304175e068e5a5740",
+    "url": "/ShareYourPlant/static/css/2.e10332d9.chunk.css"
+  },
+  {
+    "revision": "b62471c91c71c02bb205",
+    "url": "/ShareYourPlant/static/css/main.9c7dbbf3.chunk.css"
+  },
+  {
+    "revision": "087304175e068e5a5740",
+    "url": "/ShareYourPlant/static/js/2.6aef6107.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ShareYourPlant/static/js/2.6aef6107.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b62471c91c71c02bb205",
+    "url": "/ShareYourPlant/static/js/main.ec09fa1e.chunk.js"
+  },
+  {
+    "revision": "aea109f59496402ea10d",
+    "url": "/ShareYourPlant/static/js/runtime-main.efe413ba.js"
+  }
+]);
